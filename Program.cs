8693cb646e8afecda41f3362313e5e75b970f196
Riley Larson <71296500+rslarson2019@users.noreply.github.com>
2022-10-11 12:17:10 -0600

    Troubleshooting

--- Program.cs
+++ Program.cs
@@ -1,27 +1,14 @@
-﻿// Program: CSE 210 Tic Tac Toe Example
-// Author: Brother Burton
-
-using System;
-using System.Collections.Generic;
-
-/// <summary>
-/// This is a sample solution of the Tic Tac Toe program. It is not a
-/// particularly sophisticated or elegant solution to the problem, but
-/// rather it highlights a straightforward way to solve the problem
-/// for a 3x3 game.
-/// </summary>
-class Program
+﻿class TicTacToe
 {
     static void Main(string[] args)
     {
-        // Creating an object from a Class.
-        // Board is the class. new Board assigns a new class
-        Board board = new Board();
+
+        List<string> board = GetNewBoard();
         string currentPlayer = "x";
 
         while (!IsGameOver(board))
         {
-            board.print();
+            DisplayBoard(board);
 
             int choice = GetMoveChoice(currentPlayer);
             MakeMove(board, choice, currentPlayer);
@@ -29,9 +16,28 @@ class Program
             currentPlayer = GetNextPlayer(currentPlayer);
         }
 
-        board.print();
-        Console.WriteLine("Good game. Thanks for playing!");
+        DisplayBoard(board);
+        Console.WriteLine($"Good game. Thanks for playing!");
     }
+
+    /// <summary>Gets a new instance of the board with the numbers 1-9 in place. </summary>
+    /// <returns>A list of 9 strings representing each square.</returns>
+    static List<string> GetNewBoard()
+    {
+        return new List<string> {"1", "2", "3", "4", "5", "6", "7", "8", "9",};
+    }
+
+    /// <summary>Displays the board in a 3x3 grid.</summary>
+    /// <param name="board">The board</param>
+    static void DisplayBoard(List<string> board)
+    {
+        Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+        Console.WriteLine($"-+-+-");
+        Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+        Console.WriteLine($"-+-+-");
+        Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+    }
+
     /// <summary>
     /// Determines if the game is over because of a win or a tie.
     /// </summary>
@@ -39,101 +45,103 @@ class Program
     /// <returns>True if the game is over</returns>
     static bool IsGameOver(List<string> board)
     {
-        bool isGameOver = false;
-
-        if (IsWinner(board, "x") || IsWinner(board, "o") || IsTie(board))
-        {
-            isGameOver = true;
+        if (IsWinner(board, "x") == true){
+            return true;
         }
-
-        return isGameOver;
+        else if (IsWinner(board, "o") == true){
+            return true;
+        }
+        else if(IsTie(board) == true){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    /// <summary>
-    /// Determines if the provided player has a tic tac toe.
-    /// </summary>
+    /// <summary>Determines if the provided player has a tic tac toe.</summary>
     /// <param name="board">The current board</param>
     /// <param name="player">The player to check for a win</param>
     /// <returns></returns>
     static bool IsWinner(List<string> board, string player)
     {
-        // There are more elegant ways to check for a win, especially if
-        // something besides a 3x3 board were anticipated. This brute force
-        // approach is sufficient to check for the possibilities.
-
-        bool isWinner = false;
-
-        if ((board[0] == player && board[1] == player && board[2] == player)
-            || (board[3] == player && board[4] == player && board[5] == player)
-            || (board[6] == player && board[7] == player && board[8] == player)
-            || (board[0] == player && board[3] == player && board[6] == player)
-            || (board[1] == player && board[4] == player && board[7] == player)
-            || (board[2] == player && board[5] == player && board[8] == player)
-            || (board[0] == player && board[4] == player && board[8] == player)
-            || (board[2] == player && board[4] == player && board[6] == player)
-            )
-        {
-            isWinner = true;
+        //Horizontal Checks
+        if ((board[0] == player) && (board[1] == player) && (board[2] == player)){
+            return true;
+        }
+        else if((board[3] == player) && (board[4] == player) && (board[5] == player)){
+            return true;
+        }
+        else if((board[6] == player) && (board[7] == player) && (board[8] == player)){
+            return true;
         }
 
-        return isWinner; 
+        //Virtical Checks
+        else if((board[0] == player) && (board[3] == player) && (board[6] == player)){
+            return true;
+        }
+        else if((board[1] == player) && (board[4] == player) && (board[7] == player)){
+            return true;
+        }
+        else if((board[2] == player) && (board[5] == player) && (board[8] == player)){
+            return true;
+        }
+
+        //Diagonal Checks
+        else if((board[0] == player) && (board[4] == player) && (board[8] == player)){
+            return true;
+        }
+        else if((board[2] == player) && (board[4] == player) && (board[6] == player)){
+            return true;
+        }
+
+        else{
+            return false;
+        }
     }
 
-    /// <summary>
-    /// Determines if the board is full with no more moves possible.
-    /// </summary>
+    /// <summary>Determines if the board is full with no more moves possible.</summary>
     /// <param name="board">The current board.</param>
     /// <returns>True if the board is full.</returns>
     static bool IsTie(List<string> board)
     {
-        // If there is a digit, there are still moves to be made.
-        bool foundDigit = false;
-
-        foreach (string value in board)
+        int counter = 0;
+        foreach (string unit in board)
         {
-            if (char.IsDigit(value[0]))
-            {
-                foundDigit = true;
-                break;
+            if (unit == "x" || unit == "o"){
+                counter += 1;
             }
         }
-
-        return !foundDigit;
+        if (counter >= 9){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    /// <summary>
-    /// Cycles through the players (from x to o and o to x)
-    /// </summary>
+    /// <summary>Cycles through the players (from x to o and o to x)</summary>
     /// <param name="currentPlayer">The current players sign (x or o)</param>
     /// <returns>The next players sign (x or o)</returns>
     static string GetNextPlayer(string currentPlayer)
     {
-        string nextPlayer = "x";
-
-        if (currentPlayer == "x")
-        {
-            nextPlayer = "o";
+        if (currentPlayer == "o"){
+            return "x";
         }
-
-        return nextPlayer;
+        else{
+            return "o";
+        }
     }
 
-    /// <summary>
-    /// Gets the 1-based spot number associated with the user's choice.
-    /// </summary>
+    /// <summary>Gets the 1-based spot number associated with the user's choice.</summary>
     /// <param name="currentPlayer">The sign (x or o) of the current player.</param>
     /// <returns>A 1-based spot number (not a 0-based index)</returns>
     static int GetMoveChoice(string currentPlayer)
     {
-        Console.Write($"{currentPlayer}'s turn to choose a square (1-9): ");
-        string? move_string = Console.ReadLine();
-
-        if (move_string is null) {
-            return 0;
-        }
-
-        int choice = int.Parse(move_string);
-        return choice;
+        Console.WriteLine($"{currentPlayer}'s turn to choose a square (1-9): ");
+        string? input = Console.ReadLine();
+        int check = int.Parse(input);
+        return check;
     }
 
     /// <summary>
@@ -145,11 +153,9 @@ class Program
     /// <param name="currentPlayer">The current player's sign (x or o)</param>
     static void MakeMove(List<string> board, int choice, string currentPlayer)
     {
-        // Convert the 1-based spot number to a 0-based index.
-        int index = choice - 1;
-
-        // It would be good to include an additional check here to ensure that
-        // the spot is available.
-        board[index] = currentPlayer;
+        int spot = choice-1;
+        board[spot] = currentPlayer;
+        return;
     }
 }
+//contol shift p to create .vscode to run debugger
